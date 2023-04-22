654f759237d2ac199ce56020e2f772ad200cc062
Yuvraj Dagur <yuvraj.dagur928@gmail.com>
2023-04-23 04:19:55 +0530

    styling and logical changes

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 let play = false;
 var bg_music = new Audio('bg_music.mp3');
+const imgDesktop = document.getElementById('img-desktop')
+const imgMobile = document.getElementById('img-mobile')
+
 document.getElementById('play').addEventListener('click', function() {
     if (play === false) {
         bg_music.loop = true;
@@ -12,4 +15,17 @@ document.getElementById('play').addEventListener('click', function() {
         document.getElementById('icon').setAttribute('src', 'mute.png');
         play = false;
     }
+});
+
+document.addEventListener('scroll', function(event) {
+    let scroll = window.scrollY;
+    let blur = scroll * 0.05;
+    let opacity = 1 - (scroll * 0.005)
+    imgDesktop.style.filter = `blur(${blur}px)`;
+    imgDesktop.style.opacity = `${opacity}`;
+
+    let blur2 = scroll * 0.05;
+    let opacity2 = 1 - (scroll * 0.005)
+    imgMobile.style.filter = `blur(${blur2}px)`;
+    imgMobile.style.opacity = `${opacity2}`;
 });
