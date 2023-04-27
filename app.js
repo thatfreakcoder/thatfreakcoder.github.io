83b02776dc107600397c2340c5f2923d2ecd1cc9
Yuvraj Dagur <yuvraj.dagur928@gmail.com>
2023-04-27 18:02:24 +0530

    folder restructuring

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 let play = false;
-var bg_music = new Audio('bg_music.mp3');
-const imgDesktop = document.getElementById('img-desktop')
-const imgMobile = document.getElementById('img-mobile')
-
+var bg_music = new Audio('./assets/music/bg_music.mp3');
+const imgDesktop = document.getElementById("page1")
 document.getElementById('play').addEventListener('click', function() {
     if (play === false) {
         bg_music.loop = true;
         bg_music.play();
-        document.getElementById('icon').setAttribute('src', 'unmute.png');
+        document.getElementById('icon').setAttribute('src', './assets/img/unmute.png');
         play = true;
         bg_music.volume = 0.5;
     } else {
         bg_music.pause();
-        document.getElementById('icon').setAttribute('src', 'mute.png');
+        document.getElementById('icon').setAttribute('src', './assets/img/mute.png');
         play = false;
     }
 });
@@ -24,8 +22,4 @@ document.addEventListener('scroll', function(event) {
     imgDesktop.style.filter = `blur(${blur}px)`;
     imgDesktop.style.opacity = `${opacity}`;
 
-    let blur2 = scroll * 0.05;
-    let opacity2 = 1 - (scroll * 0.005)
-    imgMobile.style.filter = `blur(${blur2}px)`;
-    imgMobile.style.opacity = `${opacity2}`;
 });
